0f10484b1b5af4917d05a38fe861fecab2cd086f
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-06 19:51:50 +0200

    add flag

--- src/components/ContentsCard.vue
+++ src/components/ContentsCard.vue
@@ -2,6 +2,16 @@
 export default {
     props: {
         item: Object
+    },
+    computed: {
+        hasFlag() {
+            const availableFlags = ['it', 'en'];
+            return availableFlags.includes(this.item.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.item.original_language}.png`, import.meta.url);
+            return url.href;
+        }
     }
 };
 </script>
@@ -11,7 +21,10 @@ export default {
     <ul>
         <li>{{ item.title || item.name }}</li>
         <li>{{ item.original_title || item.original_name }}</li>
-        <li>{{ item.original_language }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" alt="">
+            <span v-else>{{ item.original_language }}</span>
+        </li>
         <li>{{ item.vote_average }}</li>
     </ul>
 </template>
